814c096cfb421f6a570245b9976856c3fe0b1e77
Mariia Zhuravlova <memkari666@gmail.com>
2024-08-14 19:15:25 +0400

    Сохранен текст последнего объявления о выставке. Если новый текст отличается от сохраненного, значит, появилось новое объявление о выставке.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ response = get(link)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 last_post = soup.find('td', class_='contentheading')
-result = last_post.getText().strip()
+last_post_text = last_post.getText().strip()
 
-last_announcement = '''CAC-CACIB FCI "RTVELI 2024" и "GEORGIAN WINNER 2024" -  Телави, Грузия'''
+last_announcement_text = '''CAC-CACIB FCI "RTVELI 2024" и "GEORGIAN WINNER 2024" -  Телави, Грузия'''
 
-if last_announcement != result:
+if last_announcement_text != last_post_text:
     print("отправить в телегу уведомление о новой выставке")
-    print('заменить last_announcement на новый текст последней выставки в БД')
+    print('заменить last_announcement_text на новый текст последней выставки в БД')
     print(soup.title)
-    print(result)
+    print(last_post_text)
